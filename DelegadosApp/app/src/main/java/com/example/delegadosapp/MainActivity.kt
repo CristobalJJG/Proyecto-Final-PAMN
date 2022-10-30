312f1a0e2ffccc625b1ab07e230414444cc3e071
Illera1999 <illerarodriguez1999@gmail.com>
2022-10-30 20:28:48 +0000

    Conectado a la BD, Falta la refactorización de los archivos

--- DelegadosApp/app/src/main/java/com/example/delegadosapp/MainActivity.kt
+++ DelegadosApp/app/src/main/java/com/example/delegadosapp/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.delegadosapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.firestore.FirebaseFirestore
+import java.util.Objects
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val db = FirebaseFirestore.getInstance()
+
+        val hashMap:HashMap<String,String> = HashMap<String,String>();
+        hashMap.put("firstName","Easy")
+        hashMap.put("lastName","Tuto")
+        hashMap.put("description","Hola esto es una prueba")
+
+        db.collection("users").add(hashMap)
+
+
+
     }
 }
