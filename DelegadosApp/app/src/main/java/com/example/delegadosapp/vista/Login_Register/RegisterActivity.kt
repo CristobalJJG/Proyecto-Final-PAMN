fd1aa2e1dd5f30caa96637dc9d0e178a558ead61
Illera1999 <illerarodriguez1999@gmail.com>
2022-11-14 13:52:18 +0000

    FirebaseAuthRegister funcional

--- DelegadosApp/app/src/main/java/com/example/delegadosapp/vista/Login_Register/RegisterActivity.kt
+++ DelegadosApp/app/src/main/java/com/example/delegadosapp/vista/Login_Register/RegisterActivity.kt
@@ -1,14 +1,21 @@
 package com.example.delegadosapp.vista.Login_Register
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
 import com.example.delegadosapp.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.example.delegadosapp.modelo.Usuario
+import com.google.firebase.firestore.FirebaseFirestore
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -17,9 +24,20 @@ class RegisterActivity : AppCompatActivity() {
     private var grado: String = ""
     private lateinit var spinner: Spinner
 
+    //Declaramos FirebaseAuth
+    private lateinit var auth: FirebaseAuth
+    //Declaramos CloudFirestore
+    val db = FirebaseFirestore.getInstance()
+    //Nuevo usuario
+    private lateinit var newUsuario: Usuario
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        //Iniciamos Firebase.auth
+        auth = Firebase.auth
+
 
         mail = intent.getStringExtra("mail").toString()
         pass = intent.getStringExtra("pass").toString()
@@ -42,7 +60,27 @@ class RegisterActivity : AppCompatActivity() {
         pass = findViewById<TextView>(R.id.txt_pass).text.toString()
         val regex = "^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$".toRegex()
         grado = spinner.selectedItem.toString()
-        if(mail.matches(regex)) showMessage(mail + "\n" + pass + "\n" + grado)
+        if(mail.matches(regex)) {
+            showMessage(mail + "\n" + pass + "\n" + grado)
+            auth.createUserWithEmailAndPassword(mail, pass)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        //updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                        //updateUI(null)
+                    }
+                }
+            newUsuario = Usuario(email = mail, grade = grado)
+            db.collection("users").document(newUsuario.getEmail()).set(newUsuario.getHashUsuario()).addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!") }
+                .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+            }
         else showMessage("Error, correo chungo")
     }
 
