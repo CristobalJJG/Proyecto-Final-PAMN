267d5e19a02911654c33a1693563afb21e04588b
Illera1999 <illerarodriguez1999@gmail.com>
2022-11-15 15:44:12 +0000

    user funcional en distintas pantallas

--- DelegadosApp/app/src/main/java/com/example/delegadosapp/vista/Publications/PublicationsActivity.kt
+++ DelegadosApp/app/src/main/java/com/example/delegadosapp/vista/Publications/PublicationsActivity.kt
@@ -11,12 +11,16 @@ import com.example.delegadosapp.controlador.AuxFunctions.showMessage
 import com.example.delegadosapp.vista.Login_Register.RegisterActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class PublicationsActivity : AppCompatActivity() {
 
 
     //Declaramos FirebaseAuth
     private lateinit var auth: FirebaseAuth
+    private lateinit var email: String
+    private lateinit var uid: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,8 +30,16 @@ class PublicationsActivity : AppCompatActivity() {
         //Iniciamos auth
         //auth = intent.get
 
-        val rol = intent.getIntExtra("rol", 0)
-        showMessage(this, "rol = "+rol)
+        /*val rol = intent.getIntExtra("rol", 0)
+        showMessage(this, "rol = "+rol)*/
+        var user = Firebase.auth.currentUser
+        user?.let {
+            // Name, email address, and profile photo Url
+            this.email = user.email.toString()
+            this.uid = user.uid.toString()
+        }
+
+        showMessage(this, "email: " + email + "\n" + "uid: " + uid)
 
 
         val recyclerView = findViewById<RecyclerView>(R.id.rv)
@@ -36,9 +48,9 @@ class PublicationsActivity : AppCompatActivity() {
         recyclerView.adapter = adapter
 
         //Si el rol es invitado-0 o usuario-1, no se muestra el botón de añadir
-        if(rol == 0 || rol == 1){
+        /*if(rol == 0 || rol == 1){
             findViewById<FloatingActionButton>(R.id.btn_addPublication).visibility = View.GONE
-        }
+        }*/
     }
 
     fun goToAddPublication(view: View) {
