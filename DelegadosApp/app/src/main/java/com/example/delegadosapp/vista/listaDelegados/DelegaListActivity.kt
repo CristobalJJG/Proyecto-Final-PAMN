b607b3118bca3c1fb4dc99b45e1b9f489bb2f248
CristóbalJJG <cristobal_jjg@hotmail.com>
2022-12-06 19:07:33 +0000

    Arreglo pequeños problemas en la lista de delegados

--- DelegadosApp/app/src/main/java/com/example/delegadosapp/vista/listaDelegados/DelegaListActivity.kt
+++ DelegadosApp/app/src/main/java/com/example/delegadosapp/vista/listaDelegados/DelegaListActivity.kt
@@ -1,7 +1,11 @@
 package com.example.delegadosapp.vista.listaDelegados
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -13,6 +17,12 @@ import com.example.delegadosapp.UsersCallback
 import com.example.delegadosapp.databinding.ActivityDelegaListBinding
 import com.example.delegadosapp.databinding.ActivityPublicationsBinding
 import com.example.delegadosapp.modelo.Usuario
+import com.example.delegadosapp.vista.login_register.LoginActivity
+import com.example.delegadosapp.vista.profile.ProfileActivity
+import com.example.delegadosapp.vista.publications.PublicationsActivity
+import com.example.delegadosapp.vista.publications.log_usuario
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.firestore.FirebaseFirestore
 
 
@@ -35,17 +45,28 @@ class DelegaListActivity : AppCompatActivity() {
                 }
             }
         )
-    }
+        //Forma de abrir el modal del menú para redirigir a todas las pantallas
+        findViewById<FloatingActionButton>(R.id.btn_modalMenu2)
+            .setOnClickListener {
+                val modal = BottomSheetDialog(this)
+                val view = layoutInflater.inflate(R.layout.menu_layout, null)
+
+                modalRegistrado(view)
+
+                modal.setContentView(view)
+                modal.show()
+            }
+        }
 
     fun onItemSelected(user: Usuario){
         showMessage(this, user.getEmail());
     }
 
-        /*
+
         fun modalRegistrado(view: View) {
 
-            view.findViewById<TextView>(R.id.txt_modalUserName).text = log_usuatio?.getNombre();
-            if (User.getRol() == 1) view.findViewById<TextView>(R.id.txt_modalCargo).text = "Alumno"
+            view.findViewById<TextView>(R.id.txt_modalUserName).text = log_usuario?.getNombre();
+            if (log_usuario?.getRol() == 1) view.findViewById<TextView>(R.id.txt_modalCargo).text = "Alumno"
             else view.findViewById<TextView>(R.id.txt_modalCargo).text = "Delegado"
 
             val btn_inicio = view.findViewById<Button>(R.id.btn_menuInicio)
@@ -67,7 +88,7 @@ class DelegaListActivity : AppCompatActivity() {
             btn_favs.visibility = View.VISIBLE
             btn_favs.setOnClickListener { showMessage(this, "Work In Progress") }
 
-            if (User.getRol() == 2) {
+            if (log_usuario?.getRol() == 2) {
                 val btn_meetings = view.findViewById<Button>(R.id.btn_menuMeetings)
                 btn_meetings.visibility = View.VISIBLE
                 btn_meetings.setOnClickListener { showMessage(this, "Work In Progress") }
@@ -87,11 +108,11 @@ class DelegaListActivity : AppCompatActivity() {
             val btn_logout = view.findViewById<Button>(R.id.btn_menuLogout)
             btn_logout.visibility = View.VISIBLE
             btn_logout.setOnClickListener {
-                log_usuatio = Usuario()
+                log_usuario = Usuario()
                 showMessage(this, "Cerrado sesión")
                 startActivity(Intent(this, LoginActivity::class.java))
             }
         }
-        */
+
 
 }
